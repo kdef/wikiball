2c28eaf9995ba0e83796c095164c8db506c1b607
Kyle DeFrancia <kyle.def@zoho.com>
2014-04-16 18:13:53 -0700

    Get only the relevant parts of the wiki article

--- wiki_parser.py
+++ wiki_parser.py
@@ -3,6 +3,15 @@ from bs4 import BeautifulSoup
 
 def get_article(html):
     parsed_html = BeautifulSoup(html)
+
+    # grab the styles
+    styles = '\n'.join(parsed_html.head.find_all(['link', 'style']))
+
+    # get the article title
     title = parsed_html.body.find('h1', attrs={'id':'firstHeading'})
+
     article = parsed_html.body.find('div', attrs={'id':'bodyContent'})
-    return unicode(parsed_html.head) + unicode(title) + unicode(article)
+    # get rid of external links
+    article.find('div', attrs={'class':'reflist'}).decompose()
+
+    return unicode(styles) + unicode(title) + unicode(article)
